d4169b3745476e7afaea8269fda62854a48c4b2a
Sourav01112 <chaudharysourav997@gmail.com>
2024-02-11 18:54:42 +0530

    implemented auth0 login and logout

---
+++ pages/api/auth/[...auth0].js
@@ -0,0 +1,4 @@
+const { handleAuth } = require("@auth0/nextjs-auth0");
+
+
+export default handleAuth()
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import Head from "next/head";
+import Link from "next/link";
+import { useUser } from '@auth0/nextjs-auth0/client'
+
 
 
 export default function Home() {
+
+  const { isLoading, error, user } = useUser()
+
+  if (isLoading) return <div>Loading.....</div>
+  if (error) return <div>{error.message}</div>
+
   return (
-   <div>
-    <Head>
-      <title>Lite ChatGPT</title>
+    <div>
+      <Head>
+        <title>Lite ChatGPT</title>
+      </Head>
       <h1>building....</h1>
-    </Head>
-   </div>
+
+      <div>
+        {!!user &&
+          <Link href="/api/auth/logout">Logout</Link>
+        }
+        {!user &&
+          <Link href="/api/auth/login">Login</Link>
+        }
+
+
+      </div>
+
+    </div>
   );
 }
